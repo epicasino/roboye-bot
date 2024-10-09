97f77bea23cda8a4d5b05472662441e1cf6e78f8
Ray Badua <rayjonathanbadua@gmail.com>
2024-10-09 15:33:44 -0700

    made choices for each gif command with an array of gifs, remove random in place for choice

--- commands/funny/cracked.js
+++ commands/funny/cracked.js
@@ -1,25 +1,27 @@
 const { SlashCommandBuilder } = require('discord.js');
 
-const gifCollection = [
-	// garrys mod
-	'https://tenor.com/view/2009cord-gmod-garrys-mod-deez-nuts-hi-gif-22165476',
-	// dont be cracked minecraft
-	'https://tenor.com/view/please-dont-be-cracked-please-dont-be-cracked-gif-21827763',
-	// fortnite
-	'https://tenor.com/view/moosh-fortnite-dont-be-cracked-troll-gif-21907195',
-	// valorant
-	'https://tenor.com/view/kian-osu-dont-be-cracked-drops-phone-valorant-gif-21994708',
-	// osu
-	'https://tenor.com/view/shigetora-cookiezi-phone-cracked-osu-phone-osu-gif-25052326',
-	// plateup
-	'https://cdn.discordapp.com/attachments/819817069281411072/1071002605804863528/please_dont_be_cracked_plateup.gif',
-];
+const cracked = require('./json/cracked.json');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('dont-be-cracked')
-		.setDescription('dont be cracked...'),
-	async execute(interaction) {
-		await interaction.reply(gifCollection[Math.floor(Math.random() * gifCollection.length)]);
-	},
+  data: new SlashCommandBuilder()
+    .setName('dont-be-cracked')
+    .setDescription('dont be cracked...')
+    .addStringOption((option) =>
+      option
+        .setName('gif')
+        .setDescription('The specific gif')
+        .setRequired(true)
+        .addChoices(
+          cracked.map(({ name, value }) => {
+            return { name, value };
+          })
+        )
+    ),
+  async execute(interaction) {
+    const gifChoice = interaction.options.getString('gif');
+    // console.log(gifChoice);
+    const gifName = cracked.find((element) => element.value === gifChoice);
+    // console.log(gifName);
+    await interaction.reply(gifName.url);
+  },
 };
