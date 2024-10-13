925cb5f662b6fd64eb859858bf82ff2245266f22
Ray Badua <rayjonathanbadua@gmail.com>
2024-10-12 18:09:18 -0700

    imgur links for gifs and pics

--- commands/funny/bigbastard.js
+++ commands/funny/bigbastard.js
@@ -5,8 +5,6 @@ module.exports = {
     .setName('big-bastard')
     .setDescription('A Big Bastard'),
   async execute(interaction) {
-    await interaction.reply(
-      'https://cdn.discordapp.com/attachments/819817069281411072/1289900229566533662/mentos_and_coke_full_crop_optimize_short.gif?ex=66fc7af3&is=66fb2973&hm=2781a92416d18d392d31c1ae6e6336a525650ffd829a8980a799e9e30d109a0e&'
-    );
+    await interaction.reply('https://i.imgur.com/tpq5Lc2.gif');
   },
 };
